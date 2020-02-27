59c239c8c54cd8c05e117981e1fa6c6eed441d1e
sinlov <sinlovppt@gmail.com>
2020-02-27 11:23:13 +0800

    let project only use zero restart at release mode

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/fvbock/endless"
 	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/GoAdminGroup/example-temp-gin/router"
@@ -62,17 +63,19 @@ func main() {
 	log.Infof("Start to listening the incoming requests on http address: %v", viper.GetString("addr"))
 	log.Infof("Sever name: %v , has start!", viper.GetString("name"))
 
-	//err := http.ListenAndServe(viper.GetString("addr"), g)
-	//if err != nil {
-	//	log.Errorf(err, "server run error %v", err)
-	//} else {
-	//	log.Infof("server run success!")
-	//}
-
-	err := endless.ListenAndServe(viper.GetString("addr"), g)
-	if err != nil {
-		log.Errorf(err, "server run error %v", err)
+	if configProject.IsDebug() {
+		err := http.ListenAndServe(viper.GetString("addr"), g)
+		if err != nil {
+			log.Errorf(err, "server run error %v", err)
+		} else {
+			log.Infof("server run success!")
+		}
 	} else {
-		log.Infof("server run success!")
+		err := endless.ListenAndServe(viper.GetString("addr"), g)
+		if err != nil {
+			log.Errorf(err, "server run error %v", err)
+		} else {
+			log.Infof("server run success!")
+		}
 	}
 }
